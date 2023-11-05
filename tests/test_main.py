3d3056426dd7c01945e5f8160fe1762ce4eab762
Coach Hall <51966143+coachhallsomeinc@users.noreply.github.com>
2023-11-05 17:40:21 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,18 @@
 # tests using pytest
 from fastapi.testclient import TestClient
+import httpx
+
+# importing the sys module
+import sys        
+ 
+# inserting the mod.py directory at 
+# position 1 in sys.path
+sys.path.insert(1, '/workspace/fastapi-rest/')    
 
 from main import app
-from routers import todos
-
-client = TestClient(app)
 
 def test_read_main():
+    client = TestClient(app)
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == "Hello World"
+    assert response.json() == {'message': 'Hello World'}
